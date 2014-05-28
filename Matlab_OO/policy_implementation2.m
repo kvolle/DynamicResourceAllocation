db17fb8c88e393f0e65f3c9733dba9e2c3dc990f
Kyle Volle <kvolle@gatech.edu>
2014-05-28 12:05:16 -0500

    Made 2 plots showing 200 runs each (with and without restart). Each run is with 5 targets, 15 robots and a perfect solution possible

--- Matlab_OO/policy_implementation2.m
+++ Matlab_OO/policy_implementation2.m
@@ -1,8 +1,8 @@
 % This is an example of simulated annealing for Dynamic Allocation
 % This is the object oriented version
-clear all
-clc
-
+%clear all
+%clc
+function err = policy_implementation2()
 % Import the policy
 policy_table = importdata('Policy_P2.txt');
 % Define the workspace
@@ -47,27 +47,33 @@ end
 
 % Set the threshold for switching based on initial allocation
 threshold = set_threshold(target_sizes,targeted);
-
 error_exists = true;
+valid_hashes = true;
 i = 0;
 tic
-
-while (error_exists && i<1000)
+streak = 0;
+while (error_exists && i<1000 && valid_hashes)
     i=i+1;
     %robot_loc = relocate(target_loc,robot_loc,targeted);
     %pause(0.15)
     %draw_step(target_loc,robot_loc,targeted);
-    for r=1:robots
-        state = robot_array(r).get_state(targeted,targets);
-        hash = get_hash(state,robots,targets);
-        robot_array(r).target = get_action(policy_table,hash);
-        targeted(r) = robot_array(r).target+1;
-        if targeted(r) <0
-            disp('ERROR');
+    %if streak<10
+        for r=1:robots
+            state = robot_array(r).get_state(targeted,targets);
+            hash = get_hash(state,robots,targets);
+            robot_array(r).target = get_action(policy_table,hash)+1;
+            targeted(r) = robot_array(r).target;
+            if targeted(r) <0
+                valid_hashes = false;
+            end
         end
-
-
-    end
+    %else
+    %    disp('Beat the streak')
+    %    for r=1:robots
+    %        robot_array(r) = ceil(rand()*targets);
+    %        targeted(r) = robot_array(r).target;
+    %    end
+    %end
     
     su = zeros(1,targets);
     for t = 1:length(target_sizes)
@@ -79,18 +85,27 @@ while (error_exists && i<1000)
     end
     error(i,:) =abs((su-target_sizes));
 
-    tot_err(i,1) = sum(error(i,:));
+    tot_err(i) = sum(error(i,:));
 
-    if tot_err(i,1)==0
+    if tot_err(i)==0
         error_exists = false;
     end
-
+    %{
+    if i >2
+        if error(i) == error(i-1)
+            streak = streak+1;
+        else
+            streak = 0;
+        end
+    end
+%}
 end
-targeted
+%targeted
 toc
-figure(2)
-plot(tot_err);
-
+%figure(2)
+%plot(tot_err);
+err =tot_err(i);
+end
 %plot(error(:,1),'r');
 %hold on
 %plot(error(:,2),'b');
